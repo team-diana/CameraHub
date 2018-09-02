31bb1702fdd940ea38711981d47a9871925b2884
gi-diana <luigi.dirado@gmail.com>
2018-09-02 23:37:40 +0200

    add cmakelists and libs

---
+++ build/CMakeFiles/rect.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nvidia/CameraHub/libs/async_client.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/async_client.cpp.o"
+  "/home/nvidia/CameraHub/libs/client.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/client.cpp.o"
+  "/home/nvidia/CameraHub/libs/connect_options.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/connect_options.cpp.o"
+  "/home/nvidia/CameraHub/libs/disconnect_options.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/disconnect_options.cpp.o"
+  "/home/nvidia/CameraHub/libs/iclient_persistence.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/iclient_persistence.cpp.o"
+  "/home/nvidia/CameraHub/libs/message.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/message.cpp.o"
+  "/home/nvidia/CameraHub/libs/response_options.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/response_options.cpp.o"
+  "/home/nvidia/CameraHub/libs/ssl_options.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/ssl_options.cpp.o"
+  "/home/nvidia/CameraHub/libs/string_collection.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/string_collection.cpp.o"
+  "/home/nvidia/CameraHub/libs/token.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/token.cpp.o"
+  "/home/nvidia/CameraHub/libs/topic.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/topic.cpp.o"
+  "/home/nvidia/CameraHub/libs/will_options.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/libs/will_options.cpp.o"
+  "/home/nvidia/CameraHub/rect.cpp" "/home/nvidia/CameraHub/build/CMakeFiles/rect.dir/rect.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/rect.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rect.dir/rect.cpp.o"
+  "CMakeFiles/rect.dir/libs/topic.cpp.o"
+  "CMakeFiles/rect.dir/libs/async_client.cpp.o"
+  "CMakeFiles/rect.dir/libs/token.cpp.o"
+  "CMakeFiles/rect.dir/libs/string_collection.cpp.o"
+  "CMakeFiles/rect.dir/libs/client.cpp.o"
+  "CMakeFiles/rect.dir/libs/iclient_persistence.cpp.o"
+  "CMakeFiles/rect.dir/libs/response_options.cpp.o"
+  "CMakeFiles/rect.dir/libs/ssl_options.cpp.o"
+  "CMakeFiles/rect.dir/libs/connect_options.cpp.o"
+  "CMakeFiles/rect.dir/libs/message.cpp.o"
+  "CMakeFiles/rect.dir/libs/disconnect_options.cpp.o"
+  "CMakeFiles/rect.dir/libs/will_options.cpp.o"
+  "rect.pdb"
+  "rect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

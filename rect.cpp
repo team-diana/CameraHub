3947538ffe2ad79f966d904126cb71793ae6d4fa
DunkelAmber <frama95@gmail.com>
2018-09-07 20:14:51 +0200

    minor

--- rect.cpp
+++ rect.cpp
@@ -4,56 +4,58 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <opencv2/imgproc/imgproc.hpp>
+
+#include "MQTTClient.h"
+
 using namespace FlyCapture2;
 
 int main()
 {
-  
-const std::string SERVER_ADDRESS	{ "10.0.0.10" };
-const std::string CLIENT_ID		{ "T0-R0 video" };
-  cv::Size bigimg(960,768);
-  cv::Size smallimg(320,256);
-  cv::Size window(1280,1024);
-  cv::Mat win_mat(window, CV_8UC3);
-  cv::Mat matPG_small(smallimg, CV_8UC3);
-  cv::Mat matbig(bigimg, CV_8UC3);
-  cv::Mat Armbuff;
-  int set=0; 
-  std::cout << "select camera /n" << " ";
-  scanf("%d", &set);
-  
-  // ****************************  ARM camera setup section *******************************
-  
-  cv::VideoCapture cap(0);
+    const std::string SERVER_ADDRESS	{ "10.0.0.10" };
+    const std::string CLIENT_ID		{ "T0-R0 video" };
+    cv::Size bigimg(960,768);
+    cv::Size smallimg(320,256);
+    cv::Size window(1280,1024);
+    cv::Mat win_mat(window, CV_8UC3);
+    cv::Mat matPG_small(smallimg, CV_8UC3);
+    cv::Mat matbig(bigimg, CV_8UC3);
+    cv::Mat Armbuff;
+    int set=0;
+    std::cout << "select camera /n" << " ";
+    scanf("%d", &set);
+
+    // ****************************  ARM camera setup section *******************************
+
+    cv::VideoCapture cap(0);
 
 
-  const double fps = cap.get(cv::CAP_PROP_FPS);
-  const int width  = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-  const int height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-  const int fourcc = cap.get(cv::CAP_PROP_FOURCC );
+    const double fps = cap.get(cv::CAP_PROP_FPS);
+    const int width  = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+    const int height = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+    const int fourcc = cap.get(cv::CAP_PROP_FOURCC );
 
-  // printf("%d", cap.get(CV_CAP_PROP_CONVERT_RGB));
-  cv::Mat matARM;
+    // printf("%d", cap.get(CV_CAP_PROP_CONVERT_RGB));
+    cv::Mat matARM;
 
 
-   //if not success, exit program
-   if (cap.isOpened() == false)
-   {
-    std::cout << "Cannot open the ARM camera" << std::endl;
-    return -1;
-   }
-     cv::Mat matARM_small;
-    
+    //if not success, exit program
+    if (cap.isOpened() == false)
+    {
+        std::cout << "Cannot open the ARM camera" << std::endl;
+        return -1;
+    }
+    cv::Mat matARM_small;
+
     Error error;
     Camera camera;
     CameraInfo camInfo;
     BusManager *bus =new BusManager();
-Error bus1 = bus->ForceAllIPAddressesAutomatically();
+    Error bus1 = bus->ForceAllIPAddressesAutomatically();
     // Connect the camera
     error = camera.Connect( 0 );
     if ( error != PGRERROR_OK )
     {
-        std::cout << "Failed to connect to camera" << std::endl;     
+        std::cout << "Failed to connect to camera" << std::endl;
         return false;
     }
 
@@ -61,88 +63,120 @@ Error bus1 = bus->ForceAllIPAddressesAutomatically();
     error = camera.GetCameraInfo( &camInfo );
     if ( error != PGRERROR_OK )
     {
-        std::cout << "Failed to get camera info from camera" << std::endl;     
+        std::cout << "Failed to get camera info from camera" << std::endl;
         return false;
     }
     std::cout << camInfo.vendorName << " "
-              << camInfo.modelName << " " 
-              << camInfo.serialNumber << std::endl;
-	
+    << camInfo.modelName << " "
+    << camInfo.serialNumber << std::endl;
+
     error = camera.StartCapture();
     if ( error == PGRERROR_ISOCH_BANDWIDTH_EXCEEDED )
     {
-        std::cout << "Bandwidth exceeded" << std::endl;     
+        std::cout << "Bandwidth exceeded" << std::endl;
         return false;
     }
     else if ( error != PGRERROR_OK )
     {
-        std::cout << "Failed to start image capture" << std::endl;     
+        std::cout << "Failed to start image capture" << std::endl;
         return false;
-    } 
-cv::VideoWriter out("appsrc ! videoconvert ! videoscale ! video/x-raw,format=YUY2,width=1280,height=1024, framerate=30/1 ! jpegenc quality=50 ! rtpjpegpay ! udpsink host=10.0.0.101 port=5000", 1800,0,30, cv::Size(1280,1024), true);
+    }
+    cv::VideoWriter out("appsrc ! videoconvert ! videoscale ! video/x-raw,format=YUY2,width=1280,height=1024, framerate=30/1 ! jpegenc quality=50 ! rtpjpegpay ! udpsink host=10.0.0.101 port=5000", 1800,0,30, cv::Size(1280,1024), true);
 
-if (out.isOpened()){
-	puts("Pipeline Opened");
-}
+    if (out.isOpened()){
+        puts("Pipeline Opened");
+    }
 
-else {
-	puts("Pipeline Broken");
-}
-char key = 0;
-int lost =0;
-        
-while(key != 'q'){
-	
-	
-	
-	Image raw;
-	Error error = camera.RetrieveBuffer(&raw);
-	if (error != PGRERROR_OK){
-		//std::cout << "network loss frame" << std::endl;
-                lost++;
-		continue;
-	}
-//	printf("Frames lost: %d \n", lost);
-	Image rgb;
-	raw.Convert( FlyCapture2::PIXEL_FORMAT_BGR, &rgb );
-	
-	unsigned int row = (double)rgb.GetReceivedDataSize()/(double)rgb.GetRows();
-	
-	cv::Mat image = cv::Mat(rgb.GetRows(), rgb.GetCols(), CV_8UC3, rgb.GetData(),row);
-	
-	//cv::imshow("image",image);
+    else {
+        puts("Pipeline Broken");
+    }
+    char key = 0;
+    int lost =0;
+
+
+    //*  MQTT
+    const std::string TOPIC { "diagnostics" };
+    const std::string PAYLOAD1 { "Hello World!" };
+
+    const char* PAYLOAD2 = "Hi there!";
+
+    // Create a client
+    mqtt::client cli("10.0.0.126", "Camera Hub");
+
+    mqtt::connect_options connOpts;
+    connOpts.set_keep_alive_interval(20);
+    connOpts.set_clean_session(true);
+
+
+    // Connect to the client
+    cli.connect(connOpts);
+
+    // Publish using a message pointer.
+    auto msg = mqtt::make_message(TOPIC, PAYLOAD1);
+    msg->set_qos(QOS);
+
+    cli.publish(msg);
+
+    // Now try with itemized publish.
+    cli.publish(TOPIC, PAYLOAD2, strlen(PAYLOAD2), 0, false);
+
+    // Disconnect
+    // cli.disconnect();
+
+    /*MQTT*/
+
+    while(key != 'q'){      // LOOP
+
+
+
+        Image raw;
+        Error error = camera.RetrieveBuffer(&raw);
+        if (error != PGRERROR_OK){
+            //std::cout << "network loss frame" << std::endl;
+            lost++;
+            continue;
+        }
+        //	printf("Frames lost: %d \n", lost);
+        Image rgb;
+        raw.Convert( FlyCapture2::PIXEL_FORMAT_BGR, &rgb );
+
+        unsigned int row = (double)rgb.GetReceivedDataSize()/(double)rgb.GetRows();
+
+        cv::Mat image = cv::Mat(rgb.GetRows(), rgb.GetCols(), CV_8UC3, rgb.GetData(),row);
+
+        //cv::imshow("image",image);
         cv::Mat matPG = cv::Mat(rgb.GetRows(), rgb.GetCols(), CV_8UC3, rgb.GetData(),row);
-	resize(matPG, matPG_small, smallimg, cv::INTER_CUBIC);
-        
-        if(cap.read(Armbuff))
-	{
-        Armbuff.copyTo(matARM);
-        //cap >> matARM;
-        
-	}
-        //Armbuff = matARM; // get a new frame from camera
- 	
-  	cv::resize(matARM, matARM_small, smallimg, cv::INTER_CUBIC );
-  
-	
-  if (set==1)
-  { 
-   	matbig=matARM;
-  }
-  else if (set==2) 
-  {
-   	matbig=matPG;
-  }
-  else 
-  {
-   	matbig=matbig;
-  }
+        resize(matPG, matPG_small, smallimg, cv::INTER_CUBIC);
 
-  cv::resize(matbig, matbig, bigimg, cv::INTER_CUBIC );
+        if(cap.read(Armbuff))
+        {
+            Armbuff.copyTo(matARM);
+            //cap >> matARM;
+
+        }
+        //Armbuff = matARM; // get a new frame from camera
+
+        cv::resize(matARM, matARM_small, smallimg, cv::INTER_CUBIC );
+
+
+        if (set==1)
+        {
+            matbig=matARM;
+        }
+        else if (set==2)
+        {
+            matbig=matPG;
+        }
+        else
+        {
+            matbig=matbig;
+        }
+
+        cv::resize(matbig, matbig, bigimg, cv::INTER_CUBIC );
 
         //cvtColor(matARM,matARM, cv::COLOR_BGR2RGB);
-	
-       
+
+
 
         matbig.copyTo(win_mat(cv::Rect(  0, 0, 960, 768)));
         matPG_small.copyTo(win_mat(cv::Rect(0,760,320,256)));
@@ -150,19 +184,17 @@ while(key != 'q'){
 
         //cv::imshow("image", win_mat);
         out.write(win_mat);
-	key= cv::waitKey(30);
-}
+        key= cv::waitKey(30);
+    }
 
- error = camera.StopCapture();
+    error = camera.StopCapture();
     if ( error != PGRERROR_OK )
     {
-        // This may fail when the camera was removed, so don't show 
+        // This may fail when the camera was removed, so don't show
         // an error message
-    }  
+    }
 
     camera.Disconnect();
 
     return 0;
-}    
-
-
+}
